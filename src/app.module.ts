3369e5180814603b82dbfaa6851042299b21b436
David Hincapie <dhincapie@condorlabs.io>
2022-11-15 01:21:53 -0500

    Setup app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
+require('dotenv').config()
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
+
+const { MONGO_URL } = process.env;
+
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    ProductsModule,
+    MongooseModule.forRoot(MONGO_URL),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
