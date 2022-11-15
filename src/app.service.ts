221d73b35b61c3bac62d297d555cff9ae98c3d86
David Hincapie <dhincapie@condorlabs.io>
2022-11-15 09:51:53 -0500

    Update health endpoint

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,10 @@
+require('dotenv').config();
 import { Injectable } from '@nestjs/common';
+const { APP_PORT } = process.env;
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  health(): string {
+    return `3A-Products_API is running on port ${APP_PORT}`;
   }
 }
